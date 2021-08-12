d6ff1e99aeb990015ff4fb56f5a3daa5d70b3577
LittleKitacho <40306561+LittleKitacho@users.noreply.github.com>
2021-08-12 19:04:33 -0400

    Rename error renderer and localize react

--- docs/js/error_render.js
+++
@@ -1,1 +0,0 @@
-var exe=false;window.onerror=(msg,url,line,col)=>{if(!exe){document.getElementById("errors").classList.toggle("errored");document.getElementById("errors").innerText="An error occoured:\r\n";exe=true}document.getElementById("errors").innerText+=url+":"+line+":"+col+" - "+msg+"\r\n"};
